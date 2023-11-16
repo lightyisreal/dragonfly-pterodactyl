8c3095d14dd7c72828b49a506c15a9a48bbf6918
ImLighty <imlighty@apexie.dev>
2023-11-16 15:31:24 +0100

    This is a test, of course

--- startup.sh
+++ startup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-if [ ! -d /mnt/server/ ]; then
-    mkdir -p /mnt/server/
-fi
-
-cd /mnt/server
-
 export GOPRIVATE=${GOPRIVATE}
+
+cd ~
+
+rm ~/.netrc ~/.wget-hsts
 
 echo "machine ${GIT_SERVER} login ${GIT_USERNAME} password ${GIT_PASSWORD}" >> ~/.netrc
 
-if [ ! -d "/mnt/server/src/" ]; then
-    git clone ${GIT_REPO} /mnt/server/src
-    cd /mnt/server/src
+if [ ! -d "~/src/" ]; then
+    git clone ${GIT_REPO} ~/src
+    cd ~/src
     git checkout ${GIT_BRANCH} || exit 1
     cd ..
 else
-    cd /mnt/server/src
+    cd ~/src
     git checkout ${GIT_BRANCH} && git pull || exit 1
     cd ..
 fi
 
-go run /mnt/server/src/${MAIN_FILE}
+go run ~/src/${MAIN_FILE}
+
+rm ~/.netrc ~/.wget-hsts
