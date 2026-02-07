74246ec53d3b5aff841d1ba6ce1086187b9827c4
Lighty <45460688+lightyisreal@users.noreply.github.com>
2026-02-07 19:34:18 +0100

    Fix comments for consistency in entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,9 +9,9 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# Replace Startup Variables
+# Replace startup variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
+# Run the server
 eval ${MODIFIED_STARTUP}
